d585788ad22542d99748e7b6b3fa8c050d95ed5f
Federico Vitale <fedevitale99@gmail.com>
2018-04-12 23:54:10 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# show-copy
+> Show unsplash api copyright infos.
+
+## Install
+```sh
+	$ npm install @splash-cli/show-copy --save
+
+	#or
+
+	$ yarn add @splash-cli/show-copy
+```
+
+## Usage
+```js
+	import showCopy from '@splash-cli/show-copy';
+
+	showCopy(photoData, info) //=> prints infos
+```
+
+
+## License
+MIT © [Federico Vitale](https://federicovitale.me)
---
+++ src/index.js
@@ -0,0 +1,48 @@
+'use strict';
+
+import parseExif from '@splash-cli/parse-exif';
+import printBlock from '@splash-cli/print-block';
+import uFormatter from '@splash-cli/unit-formatter';
+
+const showCopy = (data, info) => {
+	const user = data.user;
+
+	// Get photos infos
+	if (info) {
+		const exif = parseExif(data);
+		
+		if (exif) {
+			printBlock(chalk`{bold EXIF DATA:}`);
+		}
+
+		exif.forEach(item => {
+			console.log(chalk`{bold {yellow ${item.name.toUpperCase()}}}: ${item.value}`);
+		});
+	}
+
+	if (!info && data.description) {
+		printBlock(chalk.dim(`> ${data.description}`));
+	} else if (!info && !data.description) {
+		printBlock(chalk.dim(`> No description available.`));
+	} else if (info && data.description) {
+		console.log();
+		console.log(chalk.dim(`> ${data.description}`));
+		console.log();
+	} else {
+		console.log();
+		console.log(chalk.dim(`> No description available.`));
+		console.log();
+	}
+
+	console.log(`Downloaded: ${uFormatter(data.downloads)} times.`);
+	console.log(`Viewed: ${uFormatter(data.views)} times.`);
+	console.log(`Liked by ${uFormatter(data.likes)} users.`);
+
+	console.log();
+
+	console.log(`Shot by: ${chalk.cyan.bold(user.name)} (@${chalk.yellow(user.username)})`);
+	console.log();
+};
+
+
+module.exports = showCopy;
